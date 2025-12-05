bae494e2dd8d4ff1ab0f29414e739035e3939371
shashankmetta45 <shashankmetta45@gmail.com>
2025-12-05 14:03:41 +0530

    Add files via upload

---
+++ venky.c
@@ -0,0 +1,18 @@
+//equilateral,isosceles,or scalene triangle
+#include<stdio.h>
+int main()
+{
+int i=7;
+printf("enter a number : ");
+scanf("%d",&i);
+  switch(i){ 
+    case 1:printf("monday");break;
+    case 2:printf("tuesday");break;
+    case 3:printf("wednesday");break;
+	case 4:printf("thursday");break;
+	case 5:printf("friday");break;
+	case 6:printf("saturday");break;
+	case 7:printf("sunday");break;
+	}
+     return 0;
+}
